829da7bef39750467ea2b4c9dbdc81f8d6cce438
GAMINGNOOBdev <maho.mobilevm@outlook.de>
2025-05-10 21:59:42 +0200

    forgot to add a package qwq

--- install_packages.sh
+++ install_packages.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 yay -Sy hyprland hyprpolkitagent xdg-desktop-portal-hyprland
-sudo pacman -Sy hyprpaper waybar fastfetch swaync kservice blueman wl-clipboard kitty dolphin spotify-launcher playerctl
+sudo pacman -Sy hyprpaper waybar fastfetch swaync kservice blueman wl-clipboard kitty dolphin spotify-launcher playerctl pavucontrol
 sudo systemctl enable --now bluetooth
