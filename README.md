deeabb0a2f4b401e477aae82bcd6b8a4e1f4712c
GAMINGNOOBdev <maho.mobilevm@outlook.de>
2025-05-11 14:38:20 +0200

    Added a new theme to the installer

--- README.md
+++ README.md
@@ -1,20 +1,20 @@
 # ULTRAHYPRTHEME
-ULTRAKILL P-2 Theme for Hyprland
+ULTRAKILL Theme files for Hyprland
 
 ## Prerequisites
 You have to have archlinux. Why? Too lazy to add support to detect the package manager of choice.
 You have to have `yay`, `python3` and `git` installed.
-The extra packages such as waybar, fastfetch and hyprpaper will get installed by the install script.
+The extra packages such as hyprland, waybar, fastfetch, hyprpaper and so on will get installed by the install shell script.
 
 ## Installation
 First of all, clone this repository by running:
-`git clone https://github.com/GAMINGNOOBdev/uk_p2_hypr_theme`
+`git clone https://github.com/GAMINGNOOBdev/uk_hypr_theme`
 
 After that you can change into the cloned repository and run the install scripts:
 > Note: You have to run the `install_packages.sh` file first before running the python script!!!
 ```bash
 
-cd uk_p2_hypr_theme
+cd uk_hypr_theme
 bash install_packages.sh
 python3 install.py
 cd ..
--- install.py
+++ install.py
@@ -1,7 +1,28 @@
+#!/usr/bin/env python3
+
+#################
+###           ###
+###   SETUP   ###
+###           ###
+#################
+
+import urllib.request
+
+def download(url: str, filename: str):
+    print("Downloading '" + filename + "' from '" + url + "'")
+    urllib.request.urlretrieve(url, filename)
+
+import os
+import os.path
+if not os.path.isfile("simple_term_menu.py"):
+    print("Could not find terminal library!")
+    download("https://raw.githubusercontent.com/IngoMeyer441/simple-term-menu/refs/heads/develop/simple_term_menu.py", "simple_term_menu.py")
+
+
+
 import subprocess
 import getpass
 import shutil
-import os
 
 HOME = subprocess.getoutput("echo $HOME")
 CONFIG = f"{HOME}/.config"
@@ -20,17 +41,17 @@ PATHS = [
     WAYBAR_PATH,
 ]
 
-def copy_file(inputpath, outputpath):
-    print(f"'copying {inputpath}' --> '{outputpath}'")
-    shutil.copy(inputpath, outputpath)
-
 def init_directories():
     for directory in PATHS:
         if not os.path.isdir(directory):
             print(f"Making directory '{directory}'")
             os.mkdir(directory)
 
-def copy_config_files(inputdir, outputdir):
+def copy_file(inputpath, outputpath):
+    print(f"'copying {inputpath}' --> '{outputpath}'")
+    shutil.copy(inputpath, outputpath)
+
+def copy_files(inputdir, outputdir):
     try:
         files = subprocess.getoutput(f"ls {inputdir}").split('\n')
         for file in files:
@@ -45,13 +66,33 @@ def modify_bashrc():
         file.write("\n#ADDED BY P2 HYPRLAND THEME INSTALLER\nalias neofetch='fastfetch'\n")
 
 def main():
+    print("Select a theme to install")
+    themes = ["P-2 Blood Red Darkness", "FRAUDULENCE Golden Valley of Fools", "Cancel"]
+    option = stm.TerminalMenu(themes).show()
+    themeFolder = None
+    if option == 0:
+        themeFolder = "p2theme"
+    elif option == 1:
+        themeFolder = "fraudtheme"
+
+    if themeFolder == None:
+        print("Goodbye!")
+        exit(0)
+
     init_directories()
-    copy_file("p-2.png", f"{WALLPAPER_PATH}/p-2.png")
-    copy_config_files("hypr", HYPRLAND_PATH)
-    copy_config_files("waybar", WAYBAR_PATH)
-    copy_config_files("Scripts", SCRIPTS_PATH)
-    copy_config_files("fastfetch", FASTFETCH_PATH)
+    copy_files(f"{themeFolder}/Wallpapers", WALLPAPER_PATH)
+    copy_files(f"{themeFolder}/hypr", HYPRLAND_PATH)
+    copy_files(f"{themeFolder}/waybar", WAYBAR_PATH)
+    copy_files(f"{themeFolder}/Scripts", SCRIPTS_PATH)
+    copy_files(f"{themeFolder}/fastfetch", FASTFETCH_PATH)
     modify_bashrc()
+
+    os.system("clear")
+
+    print(f"Successfully installed '{themes[option]}'")
+    stm.TerminalMenu(["ok"]).show()
+
+    main()
 
 if __name__ == "__main__":
     main()
